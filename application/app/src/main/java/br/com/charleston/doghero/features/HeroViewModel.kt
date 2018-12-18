612b1f987ef9d300bce4d1b3b518a39726e0b708
Charleston Anjos <charleston.anjos@gmail.com>
2018-12-18 14:05:30 -0200

    get data api

--- application/app/src/main/java/br/com/charleston/doghero/features/HeroViewModel.kt
+++ application/app/src/main/java/br/com/charleston/doghero/features/HeroViewModel.kt
@@ -1,7 +1,9 @@
 package br.com.charleston.doghero.features
 
 import br.com.charleston.doghero.core.base.BaseViewModel
+import br.com.charleston.doghero.domain.DefaultObserver
 import br.com.charleston.doghero.domain.interactor.GetMyHeroes
+import br.com.charleston.doghero.domain.model.DataHeroesModel
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -11,6 +13,20 @@ class HeroViewModel @Inject constructor(
     private val getMyHeroes: GetMyHeroes
 ) : BaseViewModel() {
 
-    fun foo() {
+    fun testeApi(){
+        getMyHeroes.execute(object : DefaultObserver<DataHeroesModel>() {
+            override fun onNext(t: DataHeroesModel) {
+                super.onNext(t)
+                t.recents
+            }
+
+            override fun onError(exception: Throwable) {
+                super.onError(exception)
+            }
+
+            override fun onComplete() {
+                super.onComplete()
+            }
+        })
     }
 }
--- application/app/src/main/java/br/com/charleston/doghero/features/MyHeroesFragment.kt
+++ application/app/src/main/java/br/com/charleston/doghero/features/MyHeroesFragment.kt
@@ -1,11 +1,18 @@
 package br.com.charleston.doghero.features
 
+import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.ViewModelProviders
-import br.com.charleston.doghero.core.base.BaseFragment
 import br.com.charleston.doghero.R
+import br.com.charleston.doghero.core.base.BaseFragment
 import br.com.charleston.doghero.databinding.FragmentMyHeroesBinding
 
 class MyHeroesFragment : BaseFragment<FragmentMyHeroesBinding, HeroViewModel>() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getViewModel().testeApi()
+    }
 
     override fun getLayoutId(): Int {
         return R.layout.fragment_my_heroes
