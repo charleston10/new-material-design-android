6455b250cacefd5b36334db9665dad373be63837
Charleston Anjos <charleston.anjos@gmail.com>
2018-12-18 13:57:40 -0200

    config trust ssl

--- application/data/src/main/java/br/com/charleston/doghero/data/cloud/NetworkModule.kt
+++ application/data/src/main/java/br/com/charleston/doghero/data/cloud/NetworkModule.kt
@@ -1,5 +1,6 @@
 package br.com.charleston.doghero.data.cloud
 
+import android.annotation.SuppressLint
 import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
@@ -8,8 +9,14 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
 import javax.inject.Named
+import javax.inject.Singleton
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 @Module
 class NetworkModule {
@@ -17,14 +24,17 @@ class NetworkModule {
     @Provides
     fun okHttpClient(
         @Named(USER_AGENT_INTERCEPTOR) userAgentInterceptor: Interceptor,
-        @Named(RESPONSE_INTERCEPTOR) responseInterceptor: Interceptor
+        @Named(RESPONSE_INTERCEPTOR) responseInterceptor: Interceptor,
+        trustManager: X509TrustManager,
+        sslSocketFactory: SSLSocketFactory
     ): OkHttpClient {
         return OkHttpClient.Builder()
-            .connectTimeout(10, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(90, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
             .writeTimeout(30, TimeUnit.SECONDS)
             .addInterceptor(userAgentInterceptor)
             .addInterceptor(responseInterceptor)
+            .sslSocketFactory(sslSocketFactory, trustManager)
             .build()
     }
 
@@ -52,5 +62,30 @@ class NetworkModule {
     @Provides
     fun gsonFactory(): GsonConverterFactory {
         return GsonConverterFactory.create(GsonBuilder().create())
+    }
+
+    /**
+     * SSL Enable for github read *.txt
+     * @see (https://futurestud.io/tutorials/retrofit-2-how-to-trust-unsafe-ssl-certificates-self-signed-expired)
+     */
+    @Provides
+    @Singleton
+    fun provideSSLSocketFactory(trustManager: X509TrustManager): SSLSocketFactory {
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, arrayOf<TrustManager>(trustManager), java.security.SecureRandom())
+        return sslContext.socketFactory
+    }
+
+    @Provides
+    @Singleton
+    @SuppressLint("TrustAllX509TrustManager")
+    fun provideX509TrustManager(): X509TrustManager {
+        return object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+            override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+            override fun getAcceptedIssuers(): Array<X509Certificate> {
+                return arrayOf()
+            }
+        }
     }
 }
