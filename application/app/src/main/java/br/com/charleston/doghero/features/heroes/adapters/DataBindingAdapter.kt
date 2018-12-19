b667a2623dacfb28ff2f59811ef9ee782158c58a
Charleston Anjos <charleston.anjos@gmail.com>
2018-12-19 16:48:09 -0200

    currency format

--- application/app/src/main/java/br/com/charleston/doghero/features/heroes/adapters/DataBindingAdapter.kt
+++ application/app/src/main/java/br/com/charleston/doghero/features/heroes/adapters/DataBindingAdapter.kt
@@ -1,11 +1,14 @@
 package br.com.charleston.doghero.features.heroes.adapters
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 
-class AvatarBindingAdapter {
+import br.com.charleston.doghero.R
+
+class DataBindingAdapter {
 
     companion object {
         @JvmStatic
@@ -16,6 +19,12 @@ class AvatarBindingAdapter {
                 .load(url)
                 .apply(RequestOptions.circleCropTransform())
                 .into(view)
+        }
+
+        @JvmStatic
+        @BindingAdapter("app:currency")
+        fun formatCurrency(view: TextView, value: Double) {
+            view.text = String.format(view.context.getString(R.string.my_hero_price), value.toInt())
         }
     }
 }
